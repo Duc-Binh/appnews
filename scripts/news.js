cc2c24e8639c7bd53f8928802291c615b3ba9ba0
Duc-Binh <taducbinh18@gmail.com>
2022-06-20 23:59:06 +0700

    update setting

--- scripts/news.js
+++ scripts/news.js
@@ -1,16 +1,40 @@
+var setting = getToLocalStorage("setting") || {
+    pageSize: 5,
+    category: "tesla",
+}
+var inputSearch = $("#input-search");
+var btnSearch = $("#btn-search");
+var search = inputSearch.val();
+
+
 $("#pagination").twbsPagination({
     totalPages: 5,
     visiblePages: 5,
     onPageClick: function(event, page) {
-        newsApi(page + 1);
+        newsApi(page, search);
     },
+});
+
+btnSearch.click(function() {
+    newsApi(1, search);
 });
 
 
 
+<<<<<<< HEAD
+function newsApi(page, search) {
+    if (!search ) {
+        var url = `https://newsapi.org/v2/everything?q=${setting.category}&from=2022-05-20&sortBy=publishedAt&pagesize=${setting.pageSize}&page=${page}&apiKey=21b6139095fd4668aefcd62802b673e1
+    `;
+    } else {
+        var url = `https://newsapi.org/v2/everything?q=${search}&from=2022-05-20&sortBy=publishedAt&pagesize=${setting.pageSize}&page=${page}&apiKey=21b6139095fd4668aefcd62802b673e1
+        `;
+    }
+=======
 function newsApi(page) {
     const url = `https://newsapi.org/v2/everything?q=tesla&from=2022-05-19&sortBy=publishedAt&pagesize=5&page=${page}&apiKey=21b6139095fd4668aefcd62802b673e1
     `;
+>>>>>>> b2db1c6dbb2e79eec33f5c24a2556805aad3a07d
     $.get({
         url: url,
     }).done(function(data) {
@@ -46,4 +70,13 @@ function handleViewData(data) {
     })
 
     // console.log(articles);
+}
+
+function getToLocalStorage(arrUser) {
+    let arr = localStorage.getItem(arrUser);
+    console.log(arr);
+    if (arr) {
+        return JSON.parse(arr);
+    }
+    return null;
 }
--- scripts/setting.js
+++ scripts/setting.js
@@ -1,1 +1,28 @@
 'use strict'
+
+var inputPage = $("#input-page-size");
+var inputCategory = $("#input-category");
+var btnSetting = $("#btn-submit")
+
+class Setting {
+    constructor (pageSize, category) {
+        this.pageSize = pageSize;
+        this.category = category;
+    }
+}
+
+btnSetting.click(function() {
+    let setting = new Setting();
+    setting.pageSize = inputPage.val();
+    setting.category = inputCategory.val();
+    setToLocalStorage("setting", setting);
+    inputPage.val("");
+    inputCategory.val("general");
+});
+
+
+function setToLocalStorage(value, data) {
+    localStorage.setItem(value, JSON.stringify(data));
+}
+
+
